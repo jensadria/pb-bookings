9f90cd968a8f9c08fe254834e532b485283bdf05
jensadria <jensadria@gmail.com>
2021-05-12 00:08:39 +0800

    removed time picker and replaced with selects

--- src/App.vue
+++ src/App.vue
@@ -16,64 +16,24 @@
             required
           ></v-text-field>
           <!-- START TIME PICKER -->
-          {{ startTime }}
-          <v-menu
-            ref="menu"
-            v-model="menu1"
-            :close-on-content-click="false"
-            :nudge-right="40"
-            :return-value.sync="time"
-            transition="scale-transition"
-            offset-y
-            max-width="290px"
-            min-width="290px"
-          >
-            <template v-slot:activator="{ on, attrs }">
-              <v-text-field
-                v-model="startTime"
+          <v-row>
+            <v-col>
+              <v-select
+                :items="['10:00', '10.30']"
                 label="Start Time"
-                prepend-icon="mdi-clock-time-four-outline"
-                readonly
-                v-bind="attrs"
-                v-on="on"
-              ></v-text-field>
-            </template>
-            <v-time-picker
-              v-if="menu1"
-              v-model="startTime"
-              full-width
-              @click:minute="$refs.menu.save(time)"
-            ></v-time-picker>
-          </v-menu>
-          <!-- END TIME PICKER -->
-          <v-menu
-            ref="menu"
-            v-model="menu2"
-            :close-on-content-click="false"
-            :nudge-right="40"
-            :return-value.sync="time"
-            transition="scale-transition"
-            offset-y
-            max-width="290px"
-            min-width="290px"
-          >
-            <template v-slot:activator="{ on, attrs }">
-              <v-text-field
-                v-model="endTime"
+                filled
+              ></v-select>
+            </v-col>
+            <v-col>
+              <v-select
+                :items="['10:00', '10.30']"
                 label="End Time"
-                prepend-icon="mdi-clock-time-four-outline"
-                readonly
-                v-bind="attrs"
-                v-on="on"
-              ></v-text-field>
-            </template>
-            <v-time-picker
-              v-if="menu2"
-              v-model="endTime"
-              full-width
-              @click:minute="$refs.menu.save(time)"
-            ></v-time-picker>
-          </v-menu>
+                filled
+              ></v-select>
+            </v-col>
+          </v-row>
+          <!-- END TIME PICKER -->
+
           <div>
             <p>Snooker</p>
             <v-btn-toggle
